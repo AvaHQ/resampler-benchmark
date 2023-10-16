a93b102ac978c6a722a2e8c5b1359deabb9e5810
MatthD <matthias.dieudonne@gmail.com>
2023-10-16 13:39:53 +0200

    feat(rust-rabuto): tried in async now

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,25 @@ extern crate dotenv;
 extern crate rand;
 
 use rand::Rng;
-use rubato::{Resampler, SincFixedIn,  SincInterpolationType, SincInterpolationParameters, WindowFunction};
-use std::env;
+use rubato::{Resampler, FastFixedOut };
+
+
+
+use std::{env, vec};
 use std::fs::File;
-use std::io::{BufReader, Read, Seek, Write};
+use std::io::{BufReader, Read, Seek};
+use std::path::PathBuf;
 use std::time::Instant;
+use tokio::task::JoinSet;
+use tokio::fs::File as TokioFile;
+use tokio::io::{AsyncWriteExt, BufWriter};
 
 
 const BYTE_PER_SAMPLE: usize = 8;
-fn main() {
+#[tokio::main]
+async fn main() {
     let channels = 2;
-    let fs_out = 16000;
+    // let fs_out = 16000;
     let fs_in = 44100;
 
     dotenv::dotenv().ok(); // load.env file
@@ -28,49 +36,48 @@ fn main() {
     let nbr_input_frames = indata[0].len();
 
 
-    let _f_ratio = fs_out as f64 / fs_in as f64;
-
     // Create buffer for storing output
     let num_buffers = 50;
 
     let duration_total = Instant::now();
+    let mut write_files_futures = JoinSet::new();
+
     for index in 0..num_buffers {
         let indata_copy = indata.clone();
         let _duration = Instant::now();
         let output_filename = format!("output/output-rubato-{}.raw", index);
-        let _output_path = env::current_dir().unwrap().join(output_filename) ;
+        let output_path = env::current_dir().unwrap().join(output_filename) ;
+        let random_sample_rate = choose_random_sample_rate();
+        let ratio = random_sample_rate / 44100_f64;
+
         // Create buffer for storing output
         let mut outdata = vec![
             Vec::with_capacity(
-                2 * (nbr_input_frames as f32 * fs_out as f32 / fs_in as f32) as usize
+                2 * (nbr_input_frames as f32 * random_sample_rate as f32 / fs_in as f32) as usize
             );
             channels
         ];
 
 
         // Instanciate the re-sampler 
-        let params = SincInterpolationParameters {
-            sinc_len: 256,
-            f_cutoff: 0.95,
-            interpolation: SincInterpolationType::Linear,
-            oversampling_factor: 256,
-            window: WindowFunction::BlackmanHarris2,
-        };
-        let mut resampler = SincFixedIn::<f64>::new( // replace SincFixedIn by the async one which is fr faster
-            choose_random_sample_rate() / 44100_f64,
+        let params = rubato::PolynomialDegree::Septic;
+        println!("One file will have ratio sample {}", ratio);
+        let mut resampler = FastFixedOut::<f64>::new( 
+            ratio,
             2.0,
             params,
             1024,
             2,
         ).unwrap();
-        // Prepare
+
+        // resampler.set_resample_ratio_relative(0.8, false);
+
         let mut input_frames_next = resampler.input_frames_next();
-        let _resampler_delay = resampler.output_delay();
+        let resampler_delay = resampler.output_delay();
         let mut outbuffer = vec![vec![0.0f64; resampler.output_frames_max()]; channels];
         let mut indata_slices: Vec<&[f64]> = indata_copy.iter().map(|v| &v[..]).collect();
 
         // Process all full chunks
-
         while indata_slices[0].len() >= input_frames_next {
             let (nbr_in, nbr_out) = resampler
                 .process_into_buffer(&indata_slices, &mut outbuffer, None)
@@ -90,18 +97,21 @@ fn main() {
             append_frames(&mut outdata, &outbuffer, nbr_out);
         }
 
-        let _nbr_output_frames = (nbr_input_frames as f32 * fs_out as f32 / fs_in as f32) as usize;
+        let nbr_output_frames = (nbr_input_frames as f32 * random_sample_rate as f32 / fs_in as f32) as usize;
         // println!("Re-Sample was done in {:?}.. write file to disk now", duration.elapsed());
-        // let _file_out_disk = File::create(output_path).unwrap();
-        // write_frames(
-        //     outdata,
-        //     &mut file_out_disk,
-        //     resampler_delay,
-        //     nbr_output_frames,
-        // );
+        let future = write_frames(
+            outdata,
+            output_path,
+            resampler_delay,
+            nbr_output_frames,
+        );
+        write_files_futures.spawn(future);
     }
     println!("Re-Sample of 50 file was done in {:?}.. write file to disk now", duration_total.elapsed());
-
+    while let Some(res) = write_files_futures.join_next().await {
+        let _out = res;
+        println!("wrote 1 file to disk");
+    }
 }
 
 
@@ -127,12 +137,14 @@ fn read_file<R: Read + Seek>(inbuffer: &mut R, channels: usize) -> Vec<Vec<f64>>
 }
 
 /// Helper to write all frames to a file
-fn write_frames<W: Write + Seek>(
+async fn write_frames(
     waves: Vec<Vec<f64>>,
-    output: &mut W,
+    output_path: PathBuf,
     frames_to_skip: usize,
     frames_to_write: usize,
 ) {
+    let mut file_out_disk = BufWriter::new(TokioFile::create(output_path).await.unwrap());
+
     let channels = waves.len();
     // println!("We have {}  elements in the vec to export now", frames_to_write);
     let end = frames_to_skip + frames_to_write;
@@ -140,7 +152,7 @@ fn write_frames<W: Write + Seek>(
         for wave in waves.iter().take(channels) {
             let value64 = wave[frame];
             let bytes = value64.to_le_bytes();
-            output.write_all(&bytes).unwrap();
+            file_out_disk.write_all(&bytes).await.unwrap();
         }
     }
 }
@@ -154,7 +166,7 @@ fn append_frames(buffers: &mut [Vec<f64>], additional: &[Vec<f64>], nbr_frames: 
 
 
 fn choose_random_sample_rate() -> f64 {
-    let sample_rates: Vec<f64> = vec![16000_f64, 32000_f64, 44100_f64, 48000_f64];
+    let sample_rates: Vec<f64> = vec![8000_f64, 16000_f64, 32000_f64, 44100_f64, 48000_f64];
     let random_index = rand::thread_rng().gen_range(0..sample_rates.len());
     sample_rates[random_index]
 }
